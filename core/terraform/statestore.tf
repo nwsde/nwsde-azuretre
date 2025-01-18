e43800f7a1c7c2f64ee80f830fc4b0403f1103a1
jonnyry <jonnyry@users.noreply.github.com>
2025-01-18 11:47:00 +0000

    [NWSDE #115] Disable Cosmos public network access

--- core/terraform/statestore.tf
+++ core/terraform/statestore.tf
@@ -5,9 +5,11 @@ resource "azurerm_cosmosdb_account" "tre_db_account" {
   offer_type                    = "Standard"
   kind                          = "GlobalDocumentDB"
   automatic_failover_enabled    = false
-  ip_range_filter               = "${local.azure_portal_cosmos_ips}${var.enable_local_debugging ? ",${local.myip}" : ""}"
+#  ip_range_filter               = "${local.azure_portal_cosmos_ips}${var.enable_local_debugging ? ",${local.myip}" : ""}"
   local_authentication_disabled = true
   tags                          = local.tre_core_tags
+
+  public_network_access_enabled = false
 
   dynamic "capabilities" {
     # We can't change an existing cosmos
