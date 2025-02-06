05b23814bd979aef79e485fc1d689da94a35ca85
jonnyry <jonnyry@users.noreply.github.com>
2025-02-06 12:37:01 +0000

    ***TEMP - CLOSE KEYVAULT FIREWALL (AWAITING UPSTREAM MERGE)

--- core/terraform/scripts/letsencrypt.sh
+++ core/terraform/scripts/letsencrypt.sh
@@ -3,6 +3,9 @@ set -e
 
 script_dir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 
+# shellcheck disable=SC1091
+source "$script_dir/../../../devops/scripts/kv_add_network_exception.sh"
+
 if [[ -z ${STORAGE_ACCOUNT} ]]; then
   echo "STORAGE_ACCOUNT not set"
   exit 1
